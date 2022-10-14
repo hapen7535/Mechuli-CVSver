1b47ccaaaabe0d78977dba37e079e37cdaa1ace9
Yuhee Kim <dbgml4509@naver.com>
2022-10-14 23:57:53 +0900

    design : 메인 메뉴 UI 설정

--- app/src/main/java/com/example/mechulicvs/View/LoginActivity.kt
+++ app/src/main/java/com/example/mechulicvs/View/LoginActivity.kt
@@ -1,12 +1,27 @@
 package com.example.mechulicvs.View
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mechulicvs.R
+import com.example.mechulicvs.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
     }
+
+    fun onClickLoginBtn(view : View){
+        //login 데이터 보내기
+        //response에 따라 화면 전환
+
+    }
+
 }
