4753f29b578100dd20dd64a024cc47c438277ed3
Yuhee Kim <dbgml4509@naver.com>
2022-10-17 22:57:38 +0900

    design : 회원가입 평점 UI 설정

---
+++ app/src/main/java/com/example/mechulicvs/SignUpRatingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mechulicvs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpRatingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_rating)
+    }
+}
