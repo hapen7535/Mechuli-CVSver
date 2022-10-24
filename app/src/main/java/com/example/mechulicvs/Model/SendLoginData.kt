33b9637cb14886c32c2367672a4cbcde9d2a725a
Yuhee Kim <dbgml4509@naver.com>
2022-10-24 22:33:56 +0900

    feat : 로그인 데이터 interface 생성

--- app/src/main/java/com/example/mechulicvs/Model/SendLoginData.kt
+++ app/src/main/java/com/example/mechulicvs/Model/SendLoginData.kt
@@ -1,7 +1,16 @@
 package com.example.mechulicvs.Model
 
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface SendLoginData {
 
-
+    @FormUrlEncoded
+    @POST("/users")
+    suspend fun userLogin(
+        @Field("user_id") id : String,
+        @Field("user_pw") pw : String,
+    ) : LoginDataResult
 
 }
