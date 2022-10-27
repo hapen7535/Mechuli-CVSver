37e5a6251dace4e19db373f8bd125321a67e0ded
Yuhee Kim <dbgml4509@naver.com>
2022-10-27 22:54:55 +0900

    fix : Menu DataBase 수정

--- app/src/main/java/com/example/mechulicvs/Model/MenuDataBase.kt
+++ app/src/main/java/com/example/mechulicvs/Model/MenuDataBase.kt
@@ -1,9 +1,29 @@
 package com.example.mechulicvs.Model
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Menu::class], version = 1)
 abstract class MenuDataBase : RoomDatabase() {
     abstract fun menuDao() : MenuDao
+
+    companion object{
+        private var instance : MenuDataBase? = null
+
+        @Synchronized
+        fun getInstance(context : Context) : MenuDataBase? {
+            if(instance == null){
+                synchronized(MenuDataBase::class){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        MenuDataBase::class.java,
+                        "menu database"
+                    ).build()
+                }
+            }
+            return instance
+        }
+    }
 }
