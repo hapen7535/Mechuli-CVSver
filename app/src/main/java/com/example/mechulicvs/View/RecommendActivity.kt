f5fac753d3cf9cd5fb290be2b571e0e1692f97f4
Yuhee Kim <dbgml4509@naver.com>
2022-10-28 13:58:49 +0900

    feat : 회원 메뉴 버튼 기능 추가

---
+++ app/src/main/java/com/example/mechulicvs/View/RecommendActivity.kt
@@ -0,0 +1,36 @@
+package com.example.mechulicvs.View//package com.example.mechulicvs.
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mechulicvs.AddRatingActivity
+import com.example.mechulicvs.GetRecomActivity
+import com.example.mechulicvs.R
+import com.example.mechulicvs.databinding.ActivityRecommendBinding
+
+class RecommendActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityRecommendBinding
+
+   override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recommend)
+
+       binding = ActivityRecommendBinding.inflate(layoutInflater)
+
+       binding.backpressBtn.setOnClickListener {
+           onBackPressedDispatcher.onBackPressed()
+       }
+
+       binding.addBtn.setOnClickListener {
+           val intent = Intent(this, AddRatingActivity::class.java)
+           startActivity(intent)
+       }
+
+       binding.recommendBtn.setOnClickListener {
+           val intent = Intent(this, GetRecomActivity::class.java)
+           startActivity(intent)
+       }
+
+ }
+}
