8cfdbffddf6cb4f9fc9a111b57f0b2415c692c92
Yuhee Kim <dbgml4509@naver.com>
2022-10-29 17:09:43 +0900

    feat : 메인 메뉴 버튼 기능 추가

--- app/src/main/java/com/example/mechulicvs/MainMenuActivity.kt
+++ app/src/main/java/com/example/mechulicvs/MainMenuActivity.kt
@@ -1,14 +1,30 @@
 package com.example.mechulicvs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mechulicvs.databinding.ActivityMainMenuBinding
 
 class MainMenuActivity : AppCompatActivity() {
 
+    lateinit var binding : ActivityMainMenuBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        binding = ActivityMainMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainmenuCommunityBtn.setOnClickListener {
+            val intent = Intent(this, CommunityActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.mainmenuRecommendBtn.setOnClickListener {
+            val intent = Intent(this, GetRecomActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
