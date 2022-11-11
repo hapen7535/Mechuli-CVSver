3d47493cec1d07fcc9da079b66964c5119345d83
Yuhee Kim <dbgml4509@naver.com>
2022-11-12 00:07:26 +0900

    feat : 커뮤니티에 메인 화면 Fragment 추가

--- app/src/main/java/com/example/mechulicvs/CommunityActivity.kt
+++ app/src/main/java/com/example/mechulicvs/CommunityActivity.kt
@@ -1,11 +1,29 @@
 package com.example.mechulicvs
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mechulicvs.databinding.ActivityCommunityBinding
+
 
 class CommunityActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityCommunityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_community)
+
+        binding = ActivityCommunityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(
+            R.id.community_fragment_frame,
+            MainCommunityFragment()
+        )
+        transaction.commit()
+
+
     }
 }
