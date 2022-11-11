bcf7318434f1c2535a923aa7d6e753e280802d28
Yuhee Kim <dbgml4509@naver.com>
2022-11-12 01:44:47 +0900

    feat : 커뮤니티 메인 화면 RecyclerView Adapter 추가

--- app/src/main/java/com/example/mechulicvs/MainCommunityFragment.kt
+++ app/src/main/java/com/example/mechulicvs/MainCommunityFragment.kt
@@ -5,15 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mechulicvs.ViewModel.RecipeListViewModel
+import com.example.mechulicvs.databinding.FragmentCommunityMainBinding
 
 class MainCommunityFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_community_main, container, false)
+    lateinit var recipeViewModel : RecipeListViewModel
+    lateinit var recipeListViewModel: RecipeListViewModel
+
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        return inflater.inflate(R.layout.fragment_community_main, container, false)
+//
+//        val binding = FragmentCommunityMainBinding.bind()
+//
+//    }
+
+    private var fragmentMainComunityBinding : FragmentCommunityMainBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentCommunityMainBinding.bind(view)
+        fragmentMainComunityBinding = binding
+        binding.postListRv
     }
 
 }
---
+++ app/src/main/java/com/example/mechulicvs/View/RecipeListAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.mechulicvs.View
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.transform.CircleCropTransformation
+import com.example.mechulicvs.Model.MenuList
+import com.example.mechulicvs.Model.Recipeinfo
+import com.example.mechulicvs.R
+
+class RecipeListAdapter(private val context: Context, val itemList : List<Recipeinfo>) : RecyclerView.Adapter<RecipeListAdapter.ViewHolder>(){
+
+    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+
+        private val postThumb = itemView.findViewById<ImageView>(R.id.post_thumbnail_iv)
+        private val postName = itemView.findViewById<TextView>(R.id.post_name_tv)
+        private val postDate = itemView.findViewById<TextView>(R.id.post_date_tv)
+        private val postWriter = itemView.findViewById<TextView>(R.id.post_nickname_tv)
+        private val commentCnt = itemView.findViewById<TextView>(R.id.comment_count_tv)
+        private val ratingAvg = itemView.findViewById<TextView>(R.id.rating_count_tv)
+
+        fun bind(datas : Recipeinfo, context : Context){
+            if(datas.recipeImgTitle != ""){
+                postThumb.load(datas.recipeImgTitle){
+                }
+            } else{
+                postThumb?.setImageResource(R.mipmap.ic_launcher) //사진 데이터 없을 시 안드로이드 기본 사진
+            }
+            postName.text = datas.recipeTitle
+            postDate.text = datas.updateTime
+            postWriter.text = datas.userNickName
+            commentCnt.text = datas.recipeReplyCount.toString()
+            ratingAvg.text = datas.recipeAverageScore.toString()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeListAdapter.ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.recommend_list_recycler_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = itemList.size
+
+    override fun onBindViewHolder(holder: RecipeListAdapter.ViewHolder, position: Int) {
+        holder.bind(itemList[position], context)
+    }
+
+
+}
