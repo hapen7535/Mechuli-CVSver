ee5d1c37ba2bb6d8e31b545a5b632983a61f499d
Yuhee Kim <dbgml4509@naver.com>
2022-11-12 00:22:13 +0900

    feat : 레시피 리스트 VM 추가

--- app/src/main/java/com/example/mechulicvs/ViewModel/RecipeListViewModel.kt
+++ app/src/main/java/com/example/mechulicvs/ViewModel/RecipeListViewModel.kt
@@ -1,7 +1,26 @@
 package com.example.mechulicvs.ViewModel
 
-class RecipeListViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.mechulicvs.Model.MenuList
+import com.example.mechulicvs.Model.Recipeinfo
+import com.example.mechulicvs.Repository.RecipeListRepository
+import com.example.mechulicvs.Repository.ResultRepository
 
+class RecipeListViewModel(application: Application) : AndroidViewModel(application) {
 
+    private var resultList = MutableLiveData<List<Recipeinfo>>()
+
+    private val context = application.applicationContext
+
+    fun getResultRepository(): LiveData<List<Recipeinfo>>? {
+        return resultList
+    }
+
+    init {
+        resultList = RecipeListRepository.getResult()!!
+    }
 
 }
