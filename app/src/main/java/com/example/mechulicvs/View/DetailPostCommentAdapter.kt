7c710b126e34abab532b2de42d13eda978af587e
Yuhee Kim <dbgml4509@naver.com>
2022-11-24 23:48:52 +0900

    feat : 포스팅 댓글 RecyclerView Item에 데이터 연결

--- app/src/main/java/com/example/mechulicvs/View/DetailPostCommentAdapter.kt
+++ app/src/main/java/com/example/mechulicvs/View/DetailPostCommentAdapter.kt
@@ -4,13 +4,15 @@ import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mechulicvs.Model.MenuList
 import com.example.mechulicvs.Model.Reply
 import com.example.mechulicvs.R
 
 class DetailPostCommentAdapter(
-private val context: Context, val imgList: List<Reply>
+private val context: Context, val commentList: List<Reply>
 ) : RecyclerView.Adapter<DetailPostCommentAdapter.ViewHolder>()
 {
     override fun onCreateViewHolder(
@@ -21,7 +23,18 @@ private val context: Context, val imgList: List<Reply>
     }
 
     inner class ViewHolder(view : View) : RecyclerView.ViewHolder(view){
-        val replyHolder : TextView = view.findViewById(R.id.comments_list_rv)
+        private val commentNickName = view.findViewById<TextView>(R.id.nick_name_tv)
+        private val commentContents = view.findViewById<TextView>(R.id.comment_contents_tv)
+        private val commentRatings = view.findViewById<RatingBar>(R.id.comment_rating_rb)
+        private val commentDate = view.findViewById<TextView>(R.id.comment_date_tv)
+
+        fun bind(datas: Reply, context: Context) {
+            commentContents.text = datas.replyContent
+            commentNickName.text = datas.replyNickName
+            commentDate.text = datas.replyCreateTime
+            commentRatings.rating = datas.replyScore.toFloat()
+        }
+
     }
 
     override fun onBindViewHolder(holder: DetailPostCommentAdapter.ViewHolder, position: Int) {
