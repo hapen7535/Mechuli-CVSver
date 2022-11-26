d33f20509add98573218a6ed2743dff0a82b1d12
Yuhee Kim <dbgml4509@naver.com>
2022-11-26 22:22:39 +0900

    fix : 댓글 Data Class 수정

--- app/src/main/java/com/example/mechulicvs/Model/CommentData.kt
+++ app/src/main/java/com/example/mechulicvs/Model/CommentData.kt
@@ -3,8 +3,6 @@ package com.example.mechulicvs.Model
 import com.google.gson.annotations.SerializedName
 
 data class CommentData (
-    @SerializedName("user_id") val userId : String,
-    @SerializedName("recipe_id") val recipeId : Int,
-    @SerializedName("reply_content") val content : String,
-    val score : Double,
+    val isSuccess : Boolean,
+    val message : String,
         )
