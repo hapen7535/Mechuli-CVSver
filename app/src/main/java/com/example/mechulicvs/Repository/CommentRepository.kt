7bf2e534eeda577dcc6a3937bf5e312158a65e46
Yuhee Kim <dbgml4509@naver.com>
2022-11-27 17:34:32 +0900

    feat : comment 데이터 레포지토리 생성

---
+++ app/src/main/java/com/example/mechulicvs/Repository/CommentRepository.kt
@@ -0,0 +1,12 @@
+package com.example.mechulicvs.Repository
+
+import com.example.mechulicvs.Model.CommentData
+import com.example.mechulicvs.Model.CommentRequest
+import com.example.mechulicvs.Model.SendCommentData
+import retrofit2.Response
+
+class CommentRepository {
+    suspend fun userComment(commentRequest: CommentRequest): Response<CommentData>?{
+        return SendCommentData?.getCommentApi().sendComment(commentRequest = commentRequest)
+    }
+}
